5d4a61b512bccb7b7dbcdd6cae0ed1569de6732e
Rajendra Prasad <33002868+rajendraprasadnathan@users.noreply.github.com>
2020-06-09 11:22:29 +0530

    Add files via upload

---
+++ SVM.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.plotting import scatter_matrix
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.cross_validation import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from datetime import datetime
+start=(datetime.now().minute*60*1000+datetime.now().second*1000+datetime.now().microsecond*0.001)
+sns.set(color_codes=True)
+dataset=pd.read_csv('fetaldata.csv')
+dataset.head()
+print(dataset.head())
+dataset = dataset.drop('ID',axis=1)
+dataset.head()
+print(dataset.head())
+print(dataset.shape)
+print(dataset.describe())
+print(dataset.groupby('Status').size())
+dataset.plot(kind='box', sharex=False, sharey=False)
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+classifier = SVC()
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+end = datetime.now().minute*60*1000+datetime.now().second*1000+datetime.now().microsecond*0.001
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+print('accuracy is',accuracy_score(y_pred,y_test))
+print("Execution time: %.2f ms" % (abs(end-start)))
---
+++ logistic.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from datetime import datetime
+import pandas as pd
+from pandas.plotting import scatter_matrix
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from datetime import datetime
+sns.set(color_codes=True)
+start=(datetime.now().minute*60*1000+datetime.now().second*1000+datetime.now().microsecond*0.001)
+dataset=pd.read_csv('fetaldata.csv')
+dataset.head()
+print(dataset.head())
+dataset = dataset.drop('ID',axis=1)
+dataset.head()
+print(dataset.head())
+print(dataset.shape)
+print(dataset.describe())
+print(dataset.groupby('Status').size())
+dataset.plot(kind='box', sharex=False, sharey=False)
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+end = datetime.now().minute*60*1000+datetime.now().second*1000+datetime.now().microsecond*0.001
+print('accuracy is',accuracy_score(y_pred,y_test))
+print("Execution time: %.2f ms" % (abs(end-start)))
